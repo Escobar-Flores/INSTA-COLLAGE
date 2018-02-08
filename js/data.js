83aa71f9c9e711575665e4498d8eb1f9667d9d36
Manuela <fmanuela499@gmail.com>
2018-02-08 04:35:12 -0500

    agregando data.js

---
+++ js/data.js
@@ -0,0 +1,35 @@
+let data = [
+  {id: 1,
+    uri: 'foto1.jpg'
+  },
+  {id: 2,
+    uri: 'foto2.jpg'
+  },
+  {id: 3,
+    uri: 'foto3.jpg'
+  },
+  {id: 4,
+    uri: 'foto4.jpg'
+  },
+  {id: 5,
+    uri: 'foto5.jpg'
+  },
+  {id: 6,
+    uri: 'foto6.jpg'
+  },
+  {id: 7,
+    uri: 'foto7.jpg'
+  },
+  {id: 8,
+    uri: 'foto8.jpg'
+  },
+  {id: 9,
+    uri: 'foto9.jpg'
+  },
+  {id: 10,
+    uri: 'foto10.jpg'
+  },
+  {id: 11,
+    uri: 'foto11.png'
+  },
+];
